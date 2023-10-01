62a3307354708fdab4efe90b1f1e07f2eaf4ade4
Marisa <marisavandellos@gmail.com>
2023-10-01 14:14:27 -0700

    setup react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Routes, Route, Link} from 'react-router-dom'
+import AllPlayers from './components/AllPlayers'
+import NewPlayerForm from './components/NewPlayerForm'
+import SinglePlayer from './components/SinglePlayer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <nav>
+        <Link to='/' >Home</Link>
+        <Link to='/NewPlayerForm' >New Player</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={AllPlayers} />{/*Homepage */}
+        <Route path='/NewPlayerForm' element={NewPlayerForm} />{/*Form for new players */}
+        <Route path='/SinglePlayer/:name' element={SinglePlayer} />{/*Parameterized route for individual players */}
+      </Routes>
     </>
   )
 }
