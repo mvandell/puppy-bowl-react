6ba82ace4ff7b76340dc61abd3ad2ca7ef3b9147
Marisa <marisavandellos@gmail.com>
2023-10-04 11:15:01 -0700

    started on delete button

--- src/components/AllPlayers.jsx
+++ src/components/AllPlayers.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react"
 import { Link } from "react-router-dom";
 
 const cohortName = "2306-GHP-ET-WEB-PT-SF";
-const API_URL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}`;
+const API_URL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/players`;
 
 export default function AllPlayers() {
     const [puppies, setPuppies] = useState([]);
@@ -11,7 +11,7 @@ export default function AllPlayers() {
     useEffect(() => {
         async function getPlayerList() {
             try {
-                const response = await fetch(`${API_URL}/players`);
+                const response = await fetch(`${API_URL}`);
                 const result = await response.json();
                 console.log(result.data.players);
                 setPuppies(result.data.players);
@@ -32,6 +32,18 @@ export default function AllPlayers() {
         }
     }
 
+    useEffect(() => {
+        async function removePlayer(playerId) {
+            try {
+                const response = await fetch(API_URL, {method: 'DELETE',});
+                const result = await response.json();
+                console.log(result);
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    })
+
     return (
         <>
             {/* Search Bar */}
@@ -46,6 +58,7 @@ export default function AllPlayers() {
                         return (
                             <div key={player.id}>
                                 <Link to={`/${player.id}`}>{player.name}</Link>
+                                <button onClick={removePlayer(player.id)} >Delete Player</button>
                             </div>
                         )
                     })
